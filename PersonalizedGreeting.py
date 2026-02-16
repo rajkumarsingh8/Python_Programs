228d576b53ed5ad7ee5f0152fd2e7e6032556566
rajkumarsingh8 <kumar.rajsingh18@gmail.com>
2026-02-16 13:54:22 +0530

    Assignment_1_BasicPythonComcepts

---
+++ PersonalizedGreeting.py
@@ -0,0 +1,6 @@
+f_name = input("Enter your first name: ")
+l_name = input("Enter your last name: ")
+
+full_name = f_name + " " +l_name
+
+print("Hello,", full_name + "! Welcome to the Python program")
