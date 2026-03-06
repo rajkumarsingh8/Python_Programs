dd18138495da2023e7bf0df73c2eb76f36d9414f
rajkumarsingh8 <kumar.rajsingh18@gmail.com>
2026-03-06 17:04:46 +0530

    update factorial using recursion

--- Assignment_3_Functions&Modules/FactorialUsing_Function.py
+++ Assignment_3_Functions&Modules/FactorialUsing_Function.py
@@ -1,3 +1,6 @@
+
+# Using loop
+'''
 def factorial(num):
     fact = 1
     while num >= 1:
@@ -7,3 +10,22 @@ def factorial(num):
 
 num = int(input("Enter a number: "))
 print(f"Factorial of {num} is: {factorial(num)}")
+'''
+
+# Using recursion
+
+def factorial(number):
+    """
+    Calculates factorial using recursion
+    n! = n * (n-1) * (n-2) * .... * 1
+    n! = n * (n-1)!
+    """
+    if number == 0 or number == 1:
+        return 1
+    else:
+        result = number * factorial(number-1)
+        return result
+
+print(f"Factorial of 5 is: {factorial(5)}")
+    
+
